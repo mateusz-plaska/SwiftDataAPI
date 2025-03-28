3cd4ba592b164292366aa2f0c60513c6b4d1afd6
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-03-28 17:02:20 +0100

    init: init project

---
+++ src/main/java/org/parser/swiftdata/SwiftDataApplication.java
@@ -0,0 +1,12 @@
+package org.parser.swiftdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftDataApplication.class, args);
+    }
+}
---
+++ src/test/java/org/parser/swiftdata/SwiftDataApplicationTests.java
@@ -0,0 +1,11 @@
+package org.parser.swiftdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftDataApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
