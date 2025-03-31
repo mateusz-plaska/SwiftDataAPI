df404cc96d7f93faf5bf76cbe0f30fd35ef3f068
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-03-31 21:54:22 +0200

    fix: fix main app test

--- src/test/java/org/parser/swiftdata/SwiftDataApplicationTests.java
+++ src/test/java/org/parser/swiftdata/SwiftDataApplicationTests.java
@@ -1,10 +1,15 @@
 package org.parser.swiftdata;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SwiftDataApplicationTests {
+
+    @MockBean
+    private CommandLineRunner loadSwiftData;
 
     @Test
     void contextLoads() {}
